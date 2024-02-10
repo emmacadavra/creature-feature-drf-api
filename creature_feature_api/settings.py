4d5a458c59b609dee16e11fe03730a9e4a37cf72
Emma Cadavra <emmacadavra@gmail.com>
2024-02-10 18:50:31 +0000

    Create basic reactions model

--- creature_feature_api/settings.py
+++ creature_feature_api/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
     'profiles',
     'posts',
     'comments',
+    'reactions',
+    'like_comments',
 ]
 
 MIDDLEWARE = [
--- reactions/models.py
+++ reactions/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Reaction(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='reactions')
+    # insert reaction choices
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+        unique_together = ['owner', 'post']
+
+    def __str__(self):
+        return f'{self.owner} {self.post}'
