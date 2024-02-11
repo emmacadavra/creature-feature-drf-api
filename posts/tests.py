13d8f28f40f312829c161f553263f5a317ace9b9
Emma Cadavra <emmacadavra@gmail.com>
2024-02-11 06:12:05 +0000

    Add tests for post detail view

--- posts/tests.py
+++ posts/tests.py
@@ -24,3 +24,45 @@ class PostListViewTests(APITestCase):
     def test_logged_out_user_cannot_create_post(self):
         response = self.client.post('/posts/', {'title': 'prithee be careful'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        ralof = User.objects.create_user(username='ralof', password='pass')
+        meridia = User.objects.create_user(username='meridia', password='pass')
+        Post.objects.create(
+            owner=ralof, title='hey, you...',
+            content='...youre finally awake'
+        )
+        Post.objects.create(
+            owner=meridia, title='surprise',
+            content='A NEW HAND TOUCHES THE BEACON'
+        )
+
+    def test_can_retrieve_post_with_valid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['title'], 'hey, you...')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_retrieve_post_with_invalid_id(self):
+        response = self.client.get('/posts/777/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_posts(self):
+        self.client.login(username='ralof', password='pass')
+        response = self.client.put(
+            '/posts/1/',
+            {'title': 'you were trying to cross the border, right?'}
+        )
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(
+            post.title, 'you were trying to cross the border, right?'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cannot_update_other_user_posts(self):
+        self.client.login(username='meridia', password='pass')
+        response = self.client.put(
+            '/posts/1/', {'title': 'A NEW HAND TOUCHES THE BEACON'}
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
