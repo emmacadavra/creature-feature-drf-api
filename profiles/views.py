17072ab43bc69ba61052dad5a05f3f5dd82fa049
Emma Cadavra <emmacadavra@gmail.com>
2024-02-11 03:01:00 +0000

    Extra fields added to Profiles views and serializers to display post & follower count, number of followers, and enable filtering

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from creature_feature_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -8,11 +9,26 @@ from .serializers import ProfileSerializer
 class ProfileList(generics.ListAPIView):
     # ADD DOCSTRING
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+        ).order_by('-created_on')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count', 'followers_count', 'following_count',
+        'owner__following__created_on', 'owner__followed__created_on'
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     # ADD DOCSTRING
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+        ).order_by('-created_on')
