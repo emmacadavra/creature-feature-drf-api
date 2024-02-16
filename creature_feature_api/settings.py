f24a6d6dd692fc5550cd831f7bd63a5e86896c59
Emma Cadavra <emmacadavra@gmail.com>
2024-02-16 14:42:24 +0000

    Heroku deployment failure - amend cors settings

--- creature_feature_api/settings.py
+++ creature_feature_api/settings.py
@@ -112,7 +112,13 @@ MIDDLEWARE = [
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN'),
-        os.environ.get('CLIENT_ORIGIN_DEV'),
+    ]
+
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+        r'http://127.0.0.1:3000',
     ]
 
 CORS_ALLOW_CREDENTIALS = True
