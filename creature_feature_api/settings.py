6fe7910de8ae5e379d00e3bc85d4479c680b3b64
Emma Cadavra <emmacadavra@gmail.com>
2024-02-16 15:09:00 +0000

    Successful Heroku deployment - tidy up commented out code in settings & env

--- creature_feature_api/settings.py
+++ creature_feature_api/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import re
 import dj_database_url
 
 if os.path.exists('env.py'):
@@ -115,13 +114,6 @@ if 'CLIENT_ORIGIN' in os.environ:
         os.environ.get('CLIENT_ORIGIN'),
         os.environ.get('CLIENT_ORIGIN_DEV'),
     ]
-
-# if 'CLIENT_ORIGIN_DEV' in os.environ:
-#     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-#     CORS_ALLOWED_ORIGIN_REGEXES = [
-#         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-#         r'http://127.0.0.1:3000',
-#     ]
 
 CORS_ALLOW_CREDENTIALS = True
 
